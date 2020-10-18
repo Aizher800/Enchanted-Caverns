55717661b4e4e43cad5ab424a99d3c06bb14bfce
Aizher800 <54401983+Aizher800@users.noreply.github.com>
2020-10-19 00:16:26 +0800

    Minor fixes

---
+++ Assets/SpawnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            transform.position = new Vector3(6.6f, -2.32f, 0);
+            Destroy(this);
+        }
+    }
+}
